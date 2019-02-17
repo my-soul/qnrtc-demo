84ad1861ea77163e83f94f45deec0891e51c4a78
kun <you@example.com>
2019-02-17 19:50:16 +0800

    fix

--- src/renderer/views/room/index.vue
+++ src/renderer/views/room/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="page-room">
     <h1>pili-rtc-web-version: {{ v }}</h1>
+    <button @click="joinRoom">joinRoom</button>
+    <div id="localtracks" style="width: 640px;"></div>
   </div>
 </template>
 
@@ -16,24 +18,14 @@ export default {
   },
   methods: {
     async joinRoom() {
+      console.log(QNRTC);
       // 初始化一个房间 Session 对象
-      const myRoom = new QNRTC.UserModeSession();
+      const myRoom = new QNRTC.TrackModeSession();
       // 这里替换成刚刚生成的 RoomToken
-      await myRoom.joinRoomWithToken(ROOMTOKEN_1);
-      console.log("joinRoom success!");
+      await myRoom.joinRoomWithToken('QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-');
+      console.log('joinRoom success!');
 
-      await publish(myRoom);
-
-      // 在这里添加
-      // 获取页面上的一个元素作为播放画面的父元素
-      const localElement = document.getElementById("localtracks");
-      // 遍历本地采集的 Track 对象
-      for (const localTrack of localTracks) {
-        // 如果这是麦克风采集的音频 Track，我们就不播放它。
-        if (localTrack.info.tag === "audio") continue;
-        // 调用 Track 对象的 play 方法在这个元素下播放视频轨
-        localTrack.play(localElement, true);
-      }
+      await this.publish(myRoom);
     },
     async publish(myRoom) {
       // 我们打开了 3 个参数，即采集音频，采集视频，采集屏幕共享。
@@ -47,6 +39,17 @@ export default {
       // 将刚刚的 Track 列表发布到房间中
       await myRoom.publish(localTracks);
       console.log('publish success!');
+
+      // 在这里添加
+      // 获取页面上的一个元素作为播放画面的父元素
+      const localElement = document.getElementById('localtracks');
+      // 遍历本地采集的 Track 对象
+      for (const localTrack of localTracks) {
+        // 如果这是麦克风采集的音频 Track，我们就不播放它。
+        if (localTrack.info.tag === 'audio') continue;
+        // 调用 Track 对象的 play 方法在这个元素下播放视频轨
+        localTrack.play(localElement, true);
+      }
     }
   }
 };
