f34efa6d197915f33def1503a261521c7f3f5126
Your Name <you@example.com>
2019-02-19 17:21:35 +0800

    ch

--- server-api/src/config/config.default.ts
+++ server-api/src/config/config.default.ts
@@ -4,7 +4,7 @@ export default {
     ak: 'vvog3fXfvr_aqsjzo_nsiOuClcGZNAhSz7uXGOBS',
     sk: 'O9JZrMZ7uUMW5p7GaHrUnlkx075vj22b1VLSgYke',
     rtc: {
-      appId: 'e0yj8j0dw',
+      appId: 'e11umo41j',
     },
   },
 };
